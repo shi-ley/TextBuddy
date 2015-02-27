723dfb0900fbf367e7eabffe8b4b7237b77f1158
Shi Lei <shilei@gmail.com>
2015-02-27 13:34:28 +0800

    test if the expected output is displayed

--- src/TestBuddy2Test.java
+++ src/TestBuddy2Test.java
@@ -1,4 +1,22 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
 
 public class TestBuddy2Test {
 
+	@Test
+	public void testExecuteCommand() {
+		
+		
+		testOneCommand("display all the currently stored input", "a red hat", "display");
+		
+			}
+	
+
+	private void testOneCommand(String description, String expected, String command) {
+		assertEquals(description, expected, TextBuddy2.processInputData(command)); 
+	}
+
 }
+
